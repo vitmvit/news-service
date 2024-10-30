a5e579f3f8d71ecec90d2ec2dec524e13b1dcb84
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-10-30 15:53:15 +0300

    Revert "Dev"

--- src/main/java/ru/clevertec/news/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package ru.clevertec.news.constant;
-
-public class Constant {
-
-    public static final String OFFSET_DEFAULT = "0";
-    public static final String LIMIT_DEFAULT = "15";
-}
--- src/test/java/ru/clevertec/news/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package ru.clevertec.news.constant;
-
-public class Constant {
-
-    public static final int OFFSET = 0;
-    public static final int LIMIT = 3;
-}
